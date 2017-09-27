e4728a976402ecf81e388f152f03e092c7f5e32f
Adam Clayman <adam@clayman.org>
2017-09-27 17:39:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ function whisper(str) {
 function logShout(str) {
   console.log(str.toUpperCase());
 }
+
+function logWhisper(str) {
+  console.log(str.toLowerCase());
+}
+
+function sayHiToGrandma(str) {
+  if (str === str.toLowerCase()) {
+    return "I can't hear you!";
+  }
+  else if (str === str.toUpperCase()) {
+    return "YES INDEED!";
+  }
+  else if (str === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
